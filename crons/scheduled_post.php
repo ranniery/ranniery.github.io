029d87b7bd91df5b6c02113106a9cbec5797829f
Ranniery Holanda <email@ranniery.com.br>
2018-10-04 12:28:12 -0300

    nao rolou

--- crons/scheduled_post.php
+++
@@ -1,9 +0,0 @@
-<?php
-$base = preg_replace('|'.basename(dirname(__FILE__)).'$|', '', dirname(__FILE__));
-
-include_once $base.'lib/config.php';
-include_once $base.'App.php';
-
-$app = new App();
-$app->sendDue();
-?> 
